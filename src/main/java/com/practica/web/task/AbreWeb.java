cc819e373a34822f3580ef64dac1e3e77fbe3563
Elizabeth Figueroa Mena <efigueroam@choucairtesting.com>
2023-02-21 10:12:14 -0500

    subir repositorio

---
+++ src/main/java/com/practica/web/task/AbreWeb.java
@@ -0,0 +1,23 @@
+package com.practica.web.task;
+
+import com.practica.web.ui.UIPagina;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.Tasks;
+import net.serenitybdd.screenplay.actions.Open;
+
+public class AbreWeb implements Task{
+
+    private UIPagina paginaProductStore;
+    @Override
+    public <T extends Actor> void performAs(T actor) {
+        actor.attemptsTo(
+                Open.browserOn(paginaProductStore)
+        );
+
+    }
+
+public static AbreWeb productStore(){
+        return Tasks.instrumented(AbreWeb.class);
+    }
+}
---
+++ src/main/java/com/practica/web/ui/UIPagina.java
@@ -0,0 +1,9 @@
+package com.practica.web.ui;
+
+import net.serenitybdd.core.pages.PageObject;
+import net.thucydides.core.annotations.DefaultUrl;
+
+@DefaultUrl("https://www.demoblaze.com/")
+public class UIPagina extends PageObject {
+
+}
